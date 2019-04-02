95625f6195ac7b7259369184e9a11e81efc3ba6d
andinhodelgado <anderson.sdelgado@gmail.com>
2019-04-02 13:56:01 -0300

    Initial commit

---
+++ app/src/main/java/br/com/usinasantafe/pem/MenuInicialActivity.java
@@ -0,0 +1,13 @@
+package br.com.usinasantafe.pem;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MenuInicialActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_inicial);
+    }
+}
